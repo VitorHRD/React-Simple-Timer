b753f341449b9a08e3cacbe78b6287e14d6ca673
VitorHRD <vrdev435@gmail.com>
2022-04-26 04:11:33 -0300

    origin

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
 import './App.css';
 
+
 function App() {
+
+  const [hours, setHours] = useState(null)
+  const [minutes, setMinutes] = useState(null)
+  const [seconds, setSeconds] = useState(null)
+  useEffect(()=>{
+    setHours(24)
+    setMinutes(0)
+    setSeconds(0)
+    let pipoca = new Date('may 30 , 2022 00.00.00').getTime()
+    console.log(pipoca)
+  },[])
+  
+  useEffect(() => {
+    setTimeout(() => {
+      if (seconds > 0) {
+        setSeconds(state => state - 1)
+      }
+      else if (seconds === 0 && minutes !== 0 ) {
+        setSeconds(59)
+        setMinutes(state => state - 1) 
+      }
+      else if (minutes === 0 && hours !== 0) {
+         setMinutes(59)
+         setSeconds(59)
+         setHours(state => state - 1)
+      }
+      else if(hours === 0){
+        setHours(23)
+        setMinutes(59)
+        setSeconds(59)
+      }
+    }, 1000)
+
+  }, [seconds])
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <span>{String(hours).padStart(2, "0")}</span>
+        <span>:</span>
+        <span>{String(minutes).padStart(2, "0")}</span>
+        <span>:</span>
+        <span>{String(seconds).padStart(2, "0")}</span>
       </header>
     </div>
   );
